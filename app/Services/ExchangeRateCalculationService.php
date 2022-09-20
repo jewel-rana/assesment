9c7e01994768e50258a055bedd9cd594e91a31cf
Jewel Rana <jewel@newroztech.com>
2022-09-20 14:23:06 +0600

    Exchange rate save to cache done

--- app/Services/ExchangeRateCalculationService.php
+++ app/Services/ExchangeRateCalculationService.php
@@ -6,6 +6,7 @@ use App\Constants\ExchangeRateConstant;
 use App\Helpers\CommonHelper;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class ExchangeRateCalculationService
 {
@@ -30,9 +31,16 @@ class ExchangeRateCalculationService
         if(Cache::has('exchange_rates')) {
             $this->rates = Cache::get('exchange_rates');
         } else {
+            Log::info('Exchange rate api called');
             $response = Http::get('https://developers.paysera.com/tasks/api/currency-exchange-rates');
             if ($response->successful()) {
-                $this->rates = $response->json()['rates'];
+                if(array_key_exists('rates', $response->json())) {
+                    Log::info('Exchange rates', $response->json());
+                    $this->rates = $response->json()['rates'];
+                    Cache::remember('exchange_rates', 120, function() use ($response) {
+                        return $response->json()['rates'];
+                    });
+                }
             }
         }
     }
